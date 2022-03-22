00d09dc53b13ca630d5af6d4660c999a66af4d30
Z3R1NH0 <98356076+Z3R1NH0@users.noreply.github.com>
2022-03-22 18:48:18 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,15 +15,22 @@ var obstacle_4;
 var obstacle_5;   
 var obstacle_6;
 
-var score;
-var jogar = 1;
-var encerrar = 0;
-var gamestate = jogar;
+var score = 0;
+var JOGAR = 1;
+var ENCERRAR = 0;
+var gamestate = JOGAR;
 
 var grupo_obstacle;
 var grupo_nuvens;
 var grupo_star;
 
+
+var trex_collide;
+
+var inv;
+
+var restart,restart_ing;
+var game_over,game_overING;
 
 
 
@@ -43,6 +50,10 @@ function preload(){
   obstacle_5 = loadImage ("obstacle5.png")
   obstacle_6 = loadImage ("obstacle6.png");
 
+  trex_collide = loadAnimation ("trex_collided.png");
+
+  restart_ING = loadImage ("RESTART.png");
+  game_overING = loadImage ("gameOver.png");
   
 }
 
@@ -59,6 +70,9 @@ function setup(){
   flor_inv = createSprite (300,200,600,10);
   flor_inv.visible = false;
 
+  
+
+
   //sprite de Trex
   trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
@@ -67,38 +81,81 @@ function setup(){
   grupo_obstacle = createGroup ();
   grupo_nuvens = createGroup ();
   grupo_star = createGroup ();
+
+  restart = createSprite (100,100);
+  restart.addImage ("acabou pra você",restart_ing);
+  restart.visible = false;
+
+  game_over = createSprite (300,100);
+  game_over.addImage ("Perdeu playboy",game_overING);
+  game_over.visible = false;
+
+  trex.addAnimation ("perdeu", trex_collide);
+
   
 }
 
 function draw(){
-  
+
   background("black");
+
+
+  fill ("White");
+  text ("Distancia Percorrida: " + score + " Metros", 25, 23);
+  
+  
 
   //console.log (trex.y);
   //console.log (frameCount);
   //console.log (Math.PI);
 
-  if (gamestate == jogar){
+  if (gamestate == JOGAR){
 
-     if (mousePressedOver (trex) && trex.y >= 180){
 
-    trex.velocityY = -11;
+    if (mousePressedOver (inv) && trex.y >= 180){
+
+    trex.velocityY = -13;
     }
 
     if (flor.x < 0){
-    flor.x = flor.width/2;
-    }
+      flor.x = flor.width/2;
+     }
+
+    score = score + Math.round (frameRate ()/60);
 
     trex.velocityY = trex.velocityY + 0.8;
-    score = score + Math.round (frameCount/1);
-    flor.velocityX = -2;
-  
-  } else if (gamestate == encerrar){
+    flor.velocityX = -( 3 + 2 * score/100);
 
-    flor.velocity = 0;
+    inv = createSprite (300,200,600,600);
+    inv.visible = false;
+
+    spawnCloud ();
+    spawnStar ();
+    spawnCactus ();
+
+    if (grupo_obstacle.isTouching (trex)) {
+      gamestate = ENCERRAR;
+    }
+
+   } else if (gamestate == ENCERRAR){
+
+    flor.velocityX = 0;
+
+    grupo_nuvens.setVelocityXEach (0);
+    grupo_obstacle.setVelocityXEach (0);
+    grupo_star.setVelocityXEach (0);
+
+    grupo_nuvens.setLifetimeEach (-1);
+    grupo_obstacle.setLifetimeEach (-1);
+    grupo_star.setLifetimeEach (-1);
+
+    trex.velocityY = 0;
+    trex.changeAnimation ("perdeu", trex_collide);
+
+    restart.visible = true;
+    game_state.visible = true;
+
   }
-
- 
   
 
   //lua.depth = cloud.depth;
@@ -108,15 +165,6 @@ function draw(){
   
   trex.collide (flor_inv);
 
-  score = 0;
-  
-  fill ("White");
-  text ("Distancia Percorrida: " + score + " Metros", 25, 23);
-
-  spawnCloud ();
-  spawnStar ();
-  spawnCactus ();
-
   drawSprites();
 }
 
@@ -124,13 +172,16 @@ function spawnCloud (){
 
   if (frameCount %60 == 0){
 
-  cloud = createSprite (575,10);
-  cloud.velocityX = -3;
-  cloud.addImage (cloud_ing);
-  cloud.scale = 0.9;
-  cloud.y = Math.round (random (10,100));
-  grupo_nuvens.add(cloud);
+   cloud = createSprite (575,10);
+   cloud.velocityX = -3;
+   cloud.addImage (cloud_ing);
+   cloud.scale = 0.9;
+   cloud.y = Math.round (random (10,100));
+   grupo_nuvens.add(cloud);
+  
   }
+
+  
 }
 
 function spawnStar (){
@@ -143,10 +194,9 @@ function spawnStar (){
     star.scale = 0.5;
     star.y = Math.round (random (10,100));
     star.lifetime = 400;
-
+    grupo_star.add(star);
   }
-
-  grupo_star.add(star);
+  
 
 }
 
@@ -156,43 +206,48 @@ function spawnCactus (){
 
   if (frameCount % Math.round (random  (60,200) ) == 0) {
 
-    obstacle = createSprite (540,170,20,50);
+    obstacle = createSprite (540,180,20,50);
+    obstacle.scale = 0.4;
+  
+    
     
 
     switch (rand){
       
       case 1: obstacle.addImage (obstacle_1);
-      obstacle.scale = 0.6;
       break;
 
       case 2: obstacle.addImage (obstacle_2);
-      obstacle.scale = 0.3;
       break;
 
       case 3: obstacle.addImage (obstacle_3);
-      obstacle.scale = 0.6;
       break;
 
       case 4: obstacle.addImage (obstacle_4);
-      obstacle.scale = 0.6;
       break;
 
       case 5: obstacle.addImage (obstacle_5);
-      obstacle.scale = 0.6;
       break;
 
       case 6: obstacle.addImage (obstacle_6);
-      obstacle.scale = 0.275;
       break;
       default: break;
       
     }
+
+    
 
     obstacle.velocityX = -3;
 
     obstacle.lifetime = 200;
 
     grupo_obstacle.add(obstacle);
+
+    if (obstacle.isTouching (grupo_obstacle)){
+
+      obstacle.x = frameCount % Math.round (random (60,200))== 0;
+      
+    }
   }
 }
 
